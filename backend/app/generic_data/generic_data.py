cd6f35fc3fbd1c5d5b7c83e5a4d0dd1db4f8a416
JOKER_SON <alreadysons@gmail.com>
2024-09-06 16:41:45 +0900

    :bug:최신 데이터로 업데이트

--- backend/app/generic_data/generic_data.py
+++ backend/app/generic_data/generic_data.py
@@ -1,30 +1,17 @@
 # generic_data.py
 from flask import Blueprint, jsonify
-from .data_value import *
+from .data_value import participants, final_score, final_score_description, category_descriptions, advice
 
 generic_data_blueprint = Blueprint('generic', __name__)
-@generic_data_blueprint.route('/generic', methods=['GET'])
 
+@generic_data_blueprint.route('/generic', methods=['GET'])
 def get_analysis_data():
-    # 실제로는 이곳에서 데이터를 분석하고 결과를 생성해야 합니다
+    # 변수로부터 데이터를 가져와서 JSON 응답을 구성
     analysis_data = {
-        'yourScore': your_score,
-        'partnerScore': partner_score,
-        'finalScore': final_score,
-        'finalScoreDescription': final_score_description,
-        'radarData': {
-            'labels': radar_labels,
-            'datasets': [
-                {
-                    'label': '대화 분석',
-                    'data': radar_data,
-                    'backgroundColor': radar_background_color,
-                    'borderColor': radar_border_color,
-                    'borderWidth': radar_border_width,
-                },
-            ],
-        },
-        'categoryDescriptions': category_descriptions,
-        'advice': advice
+        "participants": participants,
+        "finalScore": final_score,
+        "finalScoreDescription": final_score_description,
+        "categoryDescriptions": category_descriptions,
+        "advice": advice
     }
     return jsonify(analysis_data)
