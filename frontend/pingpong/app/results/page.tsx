4ae3ab0ac1fea1698eba00ee5eddf856c83875ab
hyungjulim <hjlim4u@naver.com>
2024-09-07 00:23:24 +0900

    randomcolor remove

--- frontend/pingpong/app/results/page.tsx
+++ frontend/pingpong/app/results/page.tsx
@@ -116,12 +116,6 @@
     // }
 
     const chartData = useMemo(() => {
-      const getRandomColor = () => {
-        const r = Math.floor(Math.random() * 255);
-        const g = Math.floor(Math.random() * 255);
-        const b = Math.floor(Math.random() * 255);
-        return `rgb(${r}, ${g}, ${b}, 0.2)`;
-      };
       return {
         labels: radarLabels,
         datasets: analysisData.participants.map((participant) => {
