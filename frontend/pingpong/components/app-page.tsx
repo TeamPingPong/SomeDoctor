120b1387599e20b8b5655163335476dd54d68e94
rkfcl <kwmkr6@gmail.com>
2024-09-07 09:16:34 +0900

    edit: .env.local

--- frontend/pingpong/components/app-page.tsx
+++ frontend/pingpong/components/app-page.tsx
@@ -26,7 +26,7 @@ export function AppPage() {
       formData.append('file', file)
 
       try {
-        const response = await fetch('http://localhost:5000/api/upload', {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {
           method:  'POST',
           body: formData,
         })
